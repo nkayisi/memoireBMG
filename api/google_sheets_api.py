79c174a40a70a30e3c3c217cc1567a791d941355
nkayisi <nelsonkayisirirya5@gmail.com>
2021-10-13 10:25:58 +0200

    Add or connect google sheets api to the project

---
+++ api/google_sheets_api.py
@@ -0,0 +1,10 @@
+import os
+from Google import Create_Service
+
+
+CLIENT_SECRET_FILE = 'client_secret.json'
+API_SERVICE_NAME = 'sheets'
+API_VERSION = 'v4'
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+
+# service = Create_Service(CLIENT_SECRET_FILE, API_SERVICE_NAME, API_VERSION, SCOPES)
